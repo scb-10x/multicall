0d44f5ea343ce9fa285756d63b9417e0f392bcb1
Pakorn Nathong <goddesign11294_2@hotmail.com>
2022-03-16 18:28:27 +0700

    remove bench and update complied wasm file

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,3 @@
-#![feature(test)]
-
-extern crate test;
-
 pub mod contract;
 mod error;
 pub mod msg;
@@ -9,9 +5,6 @@ pub mod querier;
 
 #[cfg(test)]
 mod unit_test;
-
-#[cfg(test)]
-mod bench;
 
 #[cfg(test)]
 pub mod mock_querier;
